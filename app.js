67ae958ac3bce7f2f424901238af83bf27eeb1bc
Florence Lauer <florence_lauer@outlook.com>
2023-02-28 12:08:25 -0500

    Remove dependency to mapbox

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import maplibreGl from 'maplibre-gl';
-import {MapboxLayer} from '@deck.gl/mapbox';
+import React, {useState, useEffect} from 'react';
+import DeckGL from '@deck.gl/react';
+import {Map} from 'react-map-gl';
 import {TripsLayer} from '@deck.gl/geo-layers';
-import {load} from '@loaders.gl/core';
 import {_GeoJSONLoader} from '@loaders.gl/json';
 import {createRoot} from 'react-dom/client';
 
@@ -15,7 +14,7 @@ function setTimestamps(data) {
   var times = []
   const interval = sum/data.geometry.coordinates.length;
   for(var i = 0; i < data.geometry.coordinates.length; i+=interval) {
-    times.push(i * 10);
+    times.push(i);
   }
   if(times.length == 0) {
     times.push(500)
@@ -24,24 +23,25 @@ function setTimestamps(data) {
   return times
 }
 
-function createMap(container, data) {  
-  const map = new maplibreGl.Map({
-    container,
-    style: 'https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json',
-    antialias: true,
-    center: [-73.494815, 45.523866],
-    zoom: 13.5,
-    bearing: 20,
-    pitch: 0
-  });
+export default function Counter() {
+  const [time, setTime] = useState(0);
+  const [animation] = useState({});
 
-  map.addControl(new maplibreGl.NavigationControl(), 'top-left');
+  const animate = () => {
+    setTime(t => (t + 1) % 1000);
+    animation.id = window.requestAnimationFrame(animate);
+  };
 
-  map.on('load', () => {
-    const layer = new MapboxLayer({
+  useEffect(() => {
+    animation.id = window.requestAnimationFrame(animate);
+    return () => window.cancelAnimationFrame(animation.id);
+  }, [animation]);
+
+  const layer = [
+    new TripsLayer({
       id: 'trips-layer',
-      type: TripsLayer,
-      data: data.features,
+      data: './geojson-route.geojson',
+      loaders: [_GeoJSONLoader],
       getPath: d => d.geometry.coordinates,
       getTimestamps: d => setTimestamps(d),
       getColor: [253, 128, 93],
@@ -49,45 +49,29 @@ function createMap(container, data) {
       widthMinPixels: 2,
       rounded: true,
       fadeTrail: true,
-      trailLength: 100,
-      currentTime: 0,
+      trailLength: 300,
+      currentTime: time,
       shadowEnabled: false
-    });
-    
-    map.addLayer(layer);
-    
-    var time = 0;
-    const animate = () => {
-      time = (time + 1) % 1800;
-      layer.setProps({
-        currentTime: time
-      });
-      window.requestAnimationFrame(animate);
-    };
+    })
+  ];
 
-    animate(time)
-  });
-
-  return {
-    update: newData => renderLayers(map, newData, time),
-    remove: () => {
-      map.remove();
-    }
+  const INITIAL_VIEW_STATE = {
+    longitude: -73.463591,
+    latitude: 45.533242,
+    zoom: 12,
+    pitch: 0,
+    bearing: 0
   };
-}
 
-async function loadAndRender(container) {
-  const tripsData = await load(
-    './geojson-route.geojson',
-    _GeoJSONLoader
+  return (
+    <DeckGL
+      layers={layer}
+      initialViewState={INITIAL_VIEW_STATE}
+      controller={true}
+    >
+      <Map reuseMaps mapStyle='https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json' preventStyleDiffing={true}/>
+    </DeckGL>
   );
-  createMap(container, tripsData);
-}
-
-export default function Counter() {
-  loadAndRender(document.getElementById('app'))
-
-  return <div className='counter'></div>
 }
 
 const root = createRoot(document.getElementById("root"));
